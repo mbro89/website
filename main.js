b575eefdaa2b53582af42b0a4e966e17600c04ff
Michał <48693761+mbro89@users.noreply.github.com>
2019-03-29 20:48:20 +0100

    menu for mobile

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var navBtn = document.querySelector("nav");
 var spanAll = document.querySelectorAll("span");
 
 navBtn.addEventListener("click", function(event){
+  navBtn.classList.toggle("nav-btn-active");
   spanAll[0].classList.toggle("active-bar-first");
   spanAll[1].classList.toggle("active-bar-second");
   spanAll[2].classList.toggle("active-bar-third");
